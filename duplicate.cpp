2bdf6f181330218ae1d8f0e2d05f65e891e91830
sayan367 <75902559+sayan367@users.noreply.github.com>
2022-08-07 12:16:38 +0530

    Add files via upload

---
+++ duplicate.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int containsduplicate(int num[],int n)
+{
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=i+1;i<=n;j++)
+        {
+            if(num[j]==num[i])
+            {
+                cout<<"True"<<endl;
+            }
+        }
+    }
+    cout<<"False"<<endl;
+}
+
+int main()
+{
+    int n=4;
+    int num[n]={1,2,2,1};
+    containsduplicate(num,n);
+    return 0;
+}
